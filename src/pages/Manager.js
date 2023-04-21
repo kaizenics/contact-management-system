280184f6e70646d94c451acf33a388d755778d11
Niko <nicosejohnsoriano@gmail.com>
2023-04-22 06:55:37 +0800

    Initial Release

--- src/pages/Manager.js
+++ src/pages/Manager.js
@@ -6,9 +6,14 @@ import Navbar from "../components/Navbar";
 function Home() {
   return (
     <>
-      <Navbar />
+      <Navbar /> 
         <div className="flex items-center justify-between w-full px-8 py-4 bg-gray-700 text-white">
           <h1 className="text-lg font-bold">Contact Manager System</h1>
+          <input type="text" className="ml-auto w-1/3 p-2 rounded-md" placeholder="Search contacts"/>
+          <Link to="/Form" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            <IoAddCircleOutline className="w-4 h-4 inline-block mr-2" />
+            Search
+          </Link>
           <Link to="/Form" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
             <IoAddCircleOutline className="w-4 h-4 inline-block mr-2" />
             Add Contact
