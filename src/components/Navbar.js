30b7e975716ded819a0d9300167cfb7c7df4d72b
Niko <nicosejohnsoriano@gmail.com>
2023-04-22 08:00:08 +0800

    Initial Release

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,7 +11,7 @@ function Navbar() {
   }
 
   return (
-    <nav className="bg-cyan-600">
+    <nav className="bg-cyan-600 transition-all duration-500">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           <div className="flex items-center">
@@ -81,5 +81,4 @@ function Navbar() {
     </nav>
   );
 }
-
 export default Navbar;
--- src/pages/Manager.js
+++ src/pages/Manager.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { IoAddCircleOutline } from "react-icons/io5";
+import { IoAddOutline, IoSearch } from "react-icons/io5";
 import Navbar from "../components/Navbar";
 
 function Home() {
@@ -9,13 +9,13 @@ function Home() {
       <Navbar /> 
         <div className="flex items-center justify-between w-full px-8 py-4 bg-gray-700 text-white">
           <h1 className="text-lg font-bold">Contact Manager System</h1>
-          <input type="text" className="ml-auto w-1/3 p-2 rounded-md" placeholder="Search contacts"/>
-          <Link to="/Form" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-            <IoAddCircleOutline className="w-4 h-4 inline-block mr-2" />
+          <input type="text" className="ml-auto w-1/3 p-2 rounded-md mr-2" placeholder="Search contacts"/>
+          <Link to="/Form" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-2">
+            <IoSearch className="w-4 h-4 inline-block mr-2" />
             Search
           </Link>
           <Link to="/Form" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-            <IoAddCircleOutline className="w-4 h-4 inline-block mr-2" />
+            <IoAddOutline className="w-4 h-4 inline-block mr-2" />
             Add Contact
           </Link>
         </div>
