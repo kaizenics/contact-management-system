ba48edae940d1bd4f59ba2cd01ee62fa95f195db
Niko <nicosejohnsoriano@gmail.com>
2023-04-23 00:46:14 +0800

    Initial Release

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,15 +5,16 @@ function Home() {
   return (
     <>
       <Navbar />
-      <div className="max-w-full pt-5 relative flex justify-center items-center">
-        <div className="flex flex-row max-w-full mx-auto bg-white rounded-md shadow-md overflow-hidden">
+      <div className="flex justify-center items-center h-screen">
+        <div className="flex flex-row w-full max-w-full mx-auto mb-96 bg-white rounded-md shadow-md overflow-hidden">
           <div className="flex-1">
             <img className="object-cover h-102 w-full z-10" src="https://www.webdew.com/hubfs/8-testimonial-video-ideas-that-will-work-for-you-and-your-business.svg" alt="" />
           </div>
-          <div className="flex-1 bg-black bg-opacity-50 p-8 text-white">
+          <div className="flex-1 bg-gradient-to-r from-gray-700 via-gray-800 to-gray-800 p-8 text-white">
             <h2 className="text-3xl font-bold mb-4">Contact Management System</h2>
-            <p className="text-lg">A powerful tool to manage your contacts and stay organized.</p>
-            <button className="bg-white text-black px-4 py-2 rounded-md mt-4">Learn More</button>
+            <p className="text-lg pb-5">A contact management system is a software tool that allows you to store and organize contact information for people and organizations that you interact with.</p>
+            <button className="bg-emerald-500 text-white  px-4 py-2 rounded-md mt-4 font-semibold mr-2 hover:bg-emerald-700">Contact Manager</button>
+            <button className="bg-emerald-500 text-white transition duration-300 ease-in-out px-4 py-2 rounded-md mt-4 font-semibold mr-2 hover:bg-emerald-700">Add Contacts</button>
           </div>
         </div>
       </div>
