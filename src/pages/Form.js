e0782ea99383a903d51579e2cd960689e0b74977
Niko <nicosejohnsoriano@gmail.com>
2023-05-03 00:54:39 +0800

    Initial Release

--- src/pages/Form.js
+++ src/pages/Form.js
@@ -9,49 +9,55 @@ export default function Form() {
     email:'',
     address:'',
     profile: '',
-})
+  });
 
-async function addContactDataHandler (data) {
-    if(data.name == ''){
-        alert('Please enter Full Name');
-        return
+  async function addContactDataHandler (data) {
+    if(data.name === ''){
+      alert('Please enter Full Name');
+      return;
     }
-    if(data.contact == ''){
-        alert('Please enter Contact Number');
-        return
+    if(data.contact === ''){
+      alert('Please enter Contact Number');
+      return;
     }
-    if(data.email == ''){
-        alert('Please enter Email Address');
-        return
+    if(data.email === ''){
+      alert('Please enter Email Address');
+      return;
     }
-    if(data.address == '') {
+    if(data.address === '') {
       alert('Please enter Address');
-      return
+      return;
     }
-    if(data.profile == '') {
+    if(data.profile === '') {
       alert('Please upload your Profile Picture');
-      return
+      return;
+    } else {
+      try {
+        var headers = {
+          Accept: "application/json",
+          "Content-Type": "application.json"
+        };
+        const url = "http://localhost/apibackend/addContact.php";
+        const res = await fetch(url, {
+          method: "POST",
+          headers: headers,
+          body: JSON.stringify(data)
+        }).then(response => response.json())
+        .then(response => {
+          alert(response[0].Message);
+        }).catch(error => {
+          console.log(error);
+        });
+      } catch(error) {
+        console.log(error);
+      }
     }
-    else{
-        try{
-            var headers = {
-                Accept: "application/json",
-                "Content-Type": "application.json"
-            };
-            const url = "http://localhost/apibackend/addContact.php";
-            const res = await fetch(url, {
-                method: "POST",
-                headers: headers,
-                body: JSON.stringify(data)
-            }).then(response => response.json())
-            .then(response => {
-                alert(response[0].Message)
-            }).catch(error => {
-                console.log(error)
-            })
-        }catch(error){console.log(error)}
-    }
-}
+  }
+
+  function handleProfilePictureChange(e) {
+    const file = e.target.files[0];
+    setData({ ...data, profile: file, });
+  }
 
   return (
     <>
@@ -63,81 +69,106 @@ async function addContactDataHandler (data) {
           </div>
           <form className="mt-8 space-y-6">
             <div className="rounded-full bg-gray-100 h-32 w-32 mx-auto mb-4">
-              {/* Profile picture display */}
+             {data.profile && <img className="object-cover rounded-full w-full h-full" src={URL.createObjectURL(data.profile)} alt="profile"/>}
             </div>
-          <div className="center rounded-md shadow-sm -space-y-px">
-            <div className="mb-4">
-              <label
-                htmlFor="full_name"
-                className="block text-gray-700 font-bold mb-2"
+            <div className="center rounded-md shadow-sm -space-y-px">
+              <div className="mb-4">
+                <label
+                  htmlFor="full_name"
+                  className="block text-gray-700 font-bold mb-2"
+                >
+                  Full Name
+                </label>
+                <input
+                  type="text"
+                  id="full_name"
+                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-cyan-500 focus:border-cyan-500 focus:z-10 sm:text-sm"
+                  placeholder="Name" 
+                  onChange={(e)=>{
+                    setData({...data, name: e.target.value});
+                  }}
+                />
+                           </div>
+              <div className="pt-4 file:mb-4">
+                <label
+                  htmlFor="contact_number"
+                  className="block text-gray-700 font-bold mb-2"
+                >
+                  Contact Number
+                </label>
+                <input
+                  type="tel"
+                  id="contact_number"
+                  name="contact_number"
+                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-cyan-500 focus:border-cyan-500 focus:z-10 sm:text-sm"
+                  placeholder="Phone Number" 
+                  onChange={(e)=>{
+                    setData({...data, contact: e.target.value});
+                  }}
+                />
+              </div>
+              <div className="pt-4 file:mb-4">
+                <label
+                  htmlFor="email"
+                  className="block text-gray-700 font-bold mb-2"
+                >
+                  Email Address
+                </label>
+                <input
+                  type="email"
+                  id="email"
+                  name="email"
+                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-cyan-500 focus:border-cyan-500 focus:z-10 sm:text-sm"
+                  placeholder="Email Address" 
+                  onChange={(e)=>{
+                    setData({...data, email: e.target.value});
+                  }}
+                />
+              </div>
+              <div className="pt-4 file:mb-4">
+                <label
+                  htmlFor="address"
+                  className="block text-gray-700 font-bold mb-2"
+                >
+                  Address
+                </label>
+                <textarea
+                  id="address"
+                  name="address"
+                  rows="3"
+                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-cyan-500 focus:border-cyan-500 focus:z-10 sm:text-sm"
+                  placeholder="Address"
+                  onChange={(e)=>{
+                    setData({...data, address: e.target.value});
+                  }}
+                ></textarea>
+              </div>
+              <div className="pt-4 file:mb-4">
+                <label
+                  htmlFor="profile_picture"
+                  className="block text-gray-700 font-bold mb-2"
+                >
+                  Profile Picture
+                </label>
+                <input
+                  type="file"
+                  id="profile_picture"
+                  name="profile_picture"
+                  accept="image/*"
+                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-cyan-500 focus:border-cyan-500 focus:z-10 sm:text-sm"
+                  onChange={handleProfilePictureChange}
+                />
+              </div>
+            </div>
+            <div className="mt-6">
+              <button
+                type="button"
+                className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-cyan-600 hover:bg-cyan-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-500"
+                onClick={() => addContactDataHandler(data)}
               >
-                Full Name
-              </label>
-              <input
-                type="text"
-                id="full_name"
-                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-cyan-500 focus:border-cyan-500 focus:z-10 sm:text-sm"
-                placeholder="Name"
-              />
+                Add Contact
+              </button>
             </div>
-            <div className="pt-4 file:mb-4">
-              <label
-                htmlFor="contact_number"
-                className="block text-gray-700 font-bold mb-2"
-              >
-                Contact Number
-              </label>
-              <input
-                type="text"
-                id="contact_number"
-                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-cyan-500 focus:border-cyan-500 focus:z-10 sm:text-sm"
-                placeholder="09XXXXXXXX"
-              />
-            </div>
-            <div className="pt-5 mb-4">
-              <label
-                htmlFor="email"
-                className="block text-gray-700 font-bold mb-2"
-              >
-                Email
-              </label>
-              <input
-                type="email"
-                id="email"
-                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-cyan-500 focus:border-cyan-500 focus:z-10 sm:text-sm"
-                placeholder="appleseed@example.com"
-              />
-            </div>
-            <div className="pt-4 mb-4">
-              <label
-                htmlFor="address"
-                className="block text-gray-700 font-bold mb-2"
-              >
-                Address
-              </label>
-              <textarea
-                id="address"
-                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-cyan-500 focus:border-cyan-500 focus:z-10 sm:text-sm"
-                rows="3"
-                placeholder="Enter Address"
-              ></textarea>
-            </div>
-          </div>
-            <div className="mb-4">
-              <label
-                htmlFor="profile_picture"
-                className="block text-gray-700 font-bold mb-2"
-              >
-                Profile Picture
-              </label>
-              <input
-                type="file"
-                id="profile_picture"
-                className="form-input w-full rounded-md border-gray-300"
-              />
-            </div>
-            <button type="submit" className="bg-blue-500 transition duration-300 ease-in-out hover:bg-blue-600 text-white py-2 px-4 rounded-lg text-center self-end">Save Contact
-            </button>
           </form>
         </div>
       </div>
